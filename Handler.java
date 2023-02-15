8e8c35c6b5a91f2dcb4ed8157c7349d4cc0ec83d
PoommipatWat <poommipat.wat@cmu.ac.th>
2023-02-15 15:26:08 +0700

    640610682

--- Handler.java
+++
@@ -1,9 +0,0 @@
-public interface Handler {
-    void setHandler(Handler nextHandler);
-    // method สำหรับกำหนด nextHandler
-    //เอาไว้ใช้กับตัวเอง process ไม่ได้ก็
-    //ส่งไปให้ nextHandler process
-
-    void process(File file);
-    String getName();
-}
